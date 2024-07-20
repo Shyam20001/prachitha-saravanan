4f948b94d47e2f6673183190d60f7110ddcfdb97
Shyam <mshayam41@gmail.com>
2024-07-20 08:31:49 +0530

    Made for Prachi

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Susmitha Maria",
+  title: "Prachitha Saravanan",
   description: "Welcome to my Portfolio",
 };
 
--- app/page.js
+++ app/page.js
@@ -23,99 +23,115 @@ export default function Home() {
           <section id="about" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white">
             <h2 className="text-3xl font-bold mb-4">About Me</h2>
             <p className="leading-relaxed text-lg">
-              Passionate about thriving in competitive environments, Susmitha Maria thrives on challenges, contributing her utmost to propel company growth. With a keen eye for innovation and dedication to excellence, she blends technical prowess with a strategic mindset, ensuring impactful solutions that drive success in every endeavor.
+              Dedicated and genuine employee with an ability to fulfill the
+              customer satisfaction. Secure a responsible career opportunity to
+              fully utilize the training and skills, while making a significant
+              contribution to develop the company.
             </p>
           </section>
           <section id="projects" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white">
             <h2 className="text-3xl font-bold mb-4">Projects</h2>
             <ProjectCard
-              title="Stack Analysis Using Web Development"
-              description="Analyzed and enhanced software systems for improved performance at JP Morgan."
+              title="Detection and Classification of OPMD and Oral Health Monitoring"
+              description="Detecting oral lesions for early detection of oral cancer."
             />
             <ProjectCard
-              title="Army Soldiers Health and Wellness Observing Sensors Embedded Safeguard Helmet Using IoT"
-              description="Monitored the health and safety of soldiers through sensor-equipped helmets."
-            />
-            <ProjectCard
-              title="Travel and Tour Website"
-              description="Created an interactive and user-friendly platform for travel and tour services using HTML, CSS, and JavaScript."
+              title="Driver Drowsiness Detector and Alert System"
+              description="Alerts the driver when they are in a drowsy state while driving."
             />
           </section>
           <section id="education" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white">
             <h2 className="text-3xl font-bold mb-4">Education</h2>
             <EducationCard
-              school="Kshatriya girl's Higher Secondary School, Virudhunagar"
-              degree="SSLC"
-              percentage="88%"
-              year="2018-2019"
+              school="Vellalar College Of Engineering & Technology"
+              degree="B.E Biomedical Engineering"
+              cgpa="7.71"
+              year="2020-2024"
             />
             <EducationCard
-              school="Kshatriya girl's Higher Secondary School, Virudhunagar"
+              school="Vellalar Matric Higher Secondary School"
               degree="HSC"
-              percentage="90.74%"
-              year="2019-2021"
+              percentage="57%"
+              year="2020"
             />
             <EducationCard
-              school="V.S.B Engineering College, Karur"
-              degree="Engineering"
-              cgpa="8.15"
-              year="2021-2023"
+              school="Vellalar Matric Higher Secondary School"
+              degree="SSLC"
+              percentage="75%"
+              year="2018"
             />
           </section>
           <section id="internship" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white">
-            <h2 className="text-3xl font-bold mb-4">Internship</h2>
+            <h2 className="text-3xl font-bold mb-4">Internships and Training</h2>
             <p className="leading-relaxed text-lg">
-              <strong>Devtown (Aug - 2023):</strong> I attended a three-month internship training at Devtown from August 2023, gaining hands-on
-              experience in software development, with a focus on web technologies and industry best practices. This
-              opportunity was sponsored by JP Morgan.
+              <strong>Hospital Training:</strong> Decontamination and Sterilization of Medical Devices at FREEDOM INSTITUTE OF MEDICAL SCIENCES, Coimbatore (7 Days).
             </p>
             <p className="leading-relaxed text-lg mt-4">
-              <strong>Ramco (July - 2023):</strong> During the internship at Ramco from July 14 to July 31, 2023, I received focused training in SQL
-              (Structured Query Language), gaining practical skills in database management and querying.
+              <strong>Hospital Training:</strong> Medical Equipment Electrical Safety Testing at LOTUS HOSPITAL, Erode (10 Days).
             </p>
-          </section>
-          <section id="paper-presentation" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white">
-            <h2 className="text-3xl font-bold mb-4">Paper Presentation</h2>
-            <p className="leading-relaxed text-lg">
-              I presented a paper on "Humanoid Robots" at Sri Ramakrishna Institute of Technology on February
-              10, 2023, discussing advancements and applications in the field of humanoid robotics.
+            <p className="leading-relaxed text-lg mt-4">
+              <strong>Hospital Training:</strong> Medical Equipment Electrical Safety Testing at CYRIX HEALTHCARE PVT LTD, Cochin (1 Day).
             </p>
           </section>
           <section id="technical-skills" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white">
             <h2 className="text-3xl font-bold mb-4">Technical Skills</h2>
             <ul className="list-disc list-inside">
-              <li>Java</li>
-              <li>SQL</li>
-              <li>HTML/CSS</li>
-              <li>JavaScript</li>
-              <li>DSA in Java</li>
+              <li>Medical Device ISO 13485</li>
+              <li>Microsoft</li>
+              <li>Work Instructions</li>
+              <li>Time Management</li>
+              <li>MDR</li>
+              <li>SAP (Systems, Applications & Products in Data Processing)</li>
             </ul>
-          </section>
-          <section id="interests" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white">
-            <h2 className="text-3xl font-bold mb-4">Interests</h2>
-            <p className="leading-relaxed text-lg">
-              Web development.
-            </p>
           </section>
           <section id="certifications" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white">
             <h2 className="text-3xl font-bold mb-4">Certifications & Training</h2>
             <ul className="list-disc list-inside">
-              <li>Certified for completion of Data Structure and Algorithm using Java in NPTEL.</li>
-              <li>Certified for completion of Web Development Bootcamp in Udemy.</li>
-              <li>Certified for completion of basics of HTML in Greatlearning.</li>
-              <li>Certified for completion of Web development in Devtown.</li>
-              <li>Certified for completion of DSA in Devtown.</li>
+              <li>Principles of Management - NPTEL | IIT KHARAGPUR (54%, July-Oct 2022)</li>
+              <li>Certification of achievement in Decontamination and Sterilization of Medical Device (77.6%, June 12, 2024)</li>
+              <li>Certificate of Participation in Medical Equipment Electrical Safety Testing (June 12, 2024)</li>
             </ul>
           </section>
-          <section id="contact" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white">
-            <h2 className="text-3xl font-bold mb-4">Contact Me</h2>
-            <ContactForm />
+          <section id="experience" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white">
+            <h2 className="text-3xl font-bold mb-4">Experience</h2>
+            <p className="leading-relaxed text-lg">
+              Currently working as REGULATORY AFFAIRS EXECUTIVE at Nice Neotech Medical Systems Private Limited, Chennai.
+            </p>
+          </section>
+          <section id="co-curricular" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white">
+            <h2 className="text-3xl font-bold mb-4">Co-Curricular Activities</h2>
+            <ul className="list-disc list-inside">
+              <li>Member of IEEE</li>
+              <li>Member of Rotract Club</li>
+            </ul>
+          </section>
+          <section id="personal-details" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white">
+            <h2 className="text-3xl font-bold mb-4">Personal Details</h2>
+            <p className="leading-relaxed text-lg">
+              <strong>Father's Name:</strong> Saravanan V
+            </p>
+            <p className="leading-relaxed text-lg">
+              <strong>Date of Birth:</strong> 18/05/2003
+            </p>
+            <p className="leading-relaxed text-lg">
+              <strong>Nationality:</strong> Indian
+            </p>
+            <p className="leading-relaxed text-lg">
+              <strong>Gender:</strong> Female
+            </p>
+          </section>
+          <section id="languages" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white">
+            <h2 className="text-3xl font-bold mb-4">Languages</h2>
+            <ul className="list-disc list-inside">
+              <li>Tamil</li>
+              <li>English</li>
+            </ul>
           </section>
           <section id="linkedin" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white flex items-center">
             <h2 className="text-3xl font-bold mb-4 flex-grow">Connect with me on LinkedIn ▶️▶️</h2>
-            <a href="https://www.linkedin.com/in/susmithamaria/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/prachitha-saravanan-488590269" target="_blank" rel="noopener noreferrer">
               <Image
-                src="https://media.licdn.com/dms/image/D5603AQG8xcvwiRFgmA/profile-displayphoto-shrink_800_800/0/1666681626392?e=1723680000&v=beta&t=kSqYx88q5adTOWut9WPltV3fTg36RSpXd2EAtbo4zu8"
+                src="https://media.licdn.com/dms/image/D5603AQF6naDloLCFEA/profile-displayphoto-shrink_800_800/0/1720767612892?e=1726704000&v=beta&t=f_Aix8swptreKv8Xpfx7vikxLfKL6Cb7tDJBnYV5MSk"
                 alt="LinkedIn Logo"
                 width={200}
                 height={100}
@@ -123,9 +139,22 @@ export default function Home() {
               />
             </a>
           </section>
+          <section id="hire-me" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white flex items-center">
+            <h2 className="text-3xl font-bold mb-4 flex-grow">Hire Me Instantly</h2>
+            <a href="tel:+919025446934" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 10l6-6m0 0l6 6m-6-6v18m9-9h-3m6 0h-3m0 0h-3m0 0h-3m0 0H6" />
+              </svg>
+              CLICK here..!!
+            </a>
+          </section>
+          <section id="contact" className="my-8 p-6 bg-gray-800 bg-opacity-50 backdrop-blur-md rounded-lg shadow-lg transform transition-transform duration-500 hover:scale-105 text-white">
+            <h2 className="text-3xl font-bold mb-4">Contact Me</h2>
+            <ContactForm />
+          </section>
         </main>
         <footer className="bg-gray-900 text-white text-center p-4 shadow-inner">
-          <p>© 2024 Susmitha M. All rights reserved.</p>
+          <p>© 2024 Prachitha S. All rights reserved.</p>
         </footer>
       </div>
     </main>
